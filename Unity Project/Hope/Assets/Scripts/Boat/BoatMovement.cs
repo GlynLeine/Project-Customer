ce28d9bd59e5fd02be221f1ae419f9e7cb2fe6f3
GlynLeine <445021@student.saxion.nl>
2019-09-04 14:26:38 +0200

    changed boat movement to raycast based movement check, Add camera frustum collision planes, Add Invisible Wall Layer, Add boat collision layer mask

--- Unity Project/Hope/Assets/Scripts/Boat/BoatMovement.cs
+++ Unity Project/Hope/Assets/Scripts/Boat/BoatMovement.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 
 public class BoatMovement : MonoBehaviour
 {
+    public LayerMask movementCollisionMask;
+
     private Plane oceanPlane;
     private new Rigidbody rigidbody;
 
     private Vector3 lastTargetPosition;
-    private Vector3 currentTargetPosition;
+    private Vector3 targetPosition;
 
-    private bool move = true;
+    private Plane[] cameraViewFrustum;
 
     void Start()
     {
+        cameraViewFrustum = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+
+        for (int i = 0; i < 6; ++i)
+        {
+            GameObject p = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            p.name = "Plane " + i.ToString();
+            p.transform.localScale = new Vector3(10, 10, 10);
+            p.transform.position = -cameraViewFrustum[i].normal * cameraViewFrustum[i].distance;
+            p.transform.rotation = Quaternion.FromToRotation(Vector3.up, cameraViewFrustum[i].normal);
+            p.AddComponent<MeshCollider>();
+            p.layer = LayerMask.NameToLayer("Invisible Wall");
+        }
+
         oceanPlane = new Plane(Vector3.up, Vector3.zero);
         rigidbody = GetComponent<Rigidbody>();
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        move = false;
-    }
-
-    private void FixedUpdate()
-    {
-        move = true;
     }
 
     void Update()
@@ -34,19 +39,23 @@ public class BoatMovement : MonoBehaviour
         Ray mouseToOceanRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (oceanPlane.Raycast(mouseToOceanRay, out entryPoint))
         {
-            lastTargetPosition = currentTargetPosition;
-            currentTargetPosition = mouseToOceanRay.GetPoint(entryPoint);
+            lastTargetPosition = targetPosition;
+            targetPosition = mouseToOceanRay.GetPoint(entryPoint);
 
             Vector3 currentPosition = rigidbody.position;
-            if (move)
+            Vector3 movement = targetPosition - currentPosition;
+
+            RaycastHit hitInfo;
+            if (!Physics.Raycast(currentPosition, movement.normalized, out hitInfo, movement.magnitude, movementCollisionMask))
             {
-                if ((lastTargetPosition - currentPosition).magnitude < 1)
-                    rigidbody.MovePosition(currentTargetPosition);
-                else
-                    rigidbody.MovePosition(currentTargetPosition);
+                rigidbody.MovePosition(targetPosition);
             }
-            else if (Vector3.Distance(lastTargetPosition, currentPosition) < Vector3.Distance(currentTargetPosition, currentPosition))
-                rigidbody.MovePosition(currentPosition + (currentTargetPosition - lastTargetPosition));
+            else
+            {
+                Vector3 movementNormal = Quaternion.AngleAxis(-90, Vector3.up) * hitInfo.normal;
+                movement = Vector3.Project(movement, movementNormal);
+                rigidbody.MovePosition(currentPosition + movement);
+            }
         }
     }
 }
