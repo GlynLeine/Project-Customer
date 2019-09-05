27cf7a60f58a287b54d3a400ec5c1adc97ca8a8c
100def <robertdayoconnor@outlook.com>
2019-09-05 12:07:20 +0200

    WIP Shader

---
+++ Unity Project/Hope/Assets/Scripts/Water Shader/Waves.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour
+{
+    //Public Properties
+    public int Dimension = 10;
+    public Octave[] Octaves;
+
+    //Mesh
+    protected MeshFilter MeshFilter;
+    protected Mesh Mesh;
+
+    private void OnValidate()
+    {
+        //Mesh Setup
+        Mesh = new Mesh();
+        Mesh.name = gameObject.name;
+
+        Mesh.vertices = GenerateVerts();
+        Mesh.triangles = GenerateTries();
+
+
+        MeshFilter = gameObject.GetComponent<MeshFilter>();
+        MeshFilter.mesh = Mesh;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Mesh Setup
+        Mesh = new Mesh();
+        Mesh.name = gameObject.name;
+
+        Mesh.vertices = GenerateVerts();
+        Mesh.triangles = GenerateTries();
+
+        MeshFilter = gameObject.GetComponent<MeshFilter>();
+        MeshFilter.mesh = Mesh;
+    }
+
+
+    private Vector3[] GenerateVerts()
+    {
+        var verts = new Vector3[(Dimension + 1) * (Dimension + 1)];
+
+        //equaly distributed verts
+        for (int x = 0; x <= Dimension; x++)
+            for (int z = 0; z <= Dimension; z++)
+                verts[index(x, z)] = new Vector3(x, 0, z);
+
+        return verts;
+    }
+
+    private int[] GenerateTries()
+    {
+        var tries = new int[Mesh.vertices.Length * 6];
+
+        //two triangles are one tile
+        for (int x = 0; x < Dimension; x++)
+        {
+            for (int z = 0; z < Dimension; z++)
+            {
+                tries[index(x, z) * 6 + 0] = index(x, z);
+                tries[index(x, z) * 6 + 1] = index(x + 1, z + 1);
+                tries[index(x, z) * 6 + 2] = index(x + 1, z);
+                tries[index(x, z) * 6 + 3] = index(x, z);
+                tries[index(x, z) * 6 + 4] = index(x, z + 1);
+                tries[index(x, z) * 6 + 5] = index(x + 1, z + 1);
+            }
+        }
+
+        return tries;
+    }
+    private int index(int x, int z)
+    {
+        return x * (Dimension + 1) + z;
+    }
+
+    private int index(float x, float z)
+    {
+        return index((int)x, (int)z);
+    }
+
+    // Runs update in editor.
+    private void OnDrawGizmos()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    [Serializable]
+    public struct Octave
+    {
+        public Vector2 speed;
+        public Vector2 scale;
+        public float height;
+        public bool alternate;
+    }
+}
