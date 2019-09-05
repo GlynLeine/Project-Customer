ab1b3808d69dc0c8efd454765ef17e8fd0352d7a
GlynLeine <445021@student.saxion.nl>
2019-09-05 14:36:10 +0200

    Add debug hud, Add Boat Types, Add Level Master, Add trash, Add Default Boat, Add FloatingObjectScript, Rename BoatMovement to BoatScript, Add Interactable Tag

---
+++ Unity Project/Hope/Assets/Scripts/Boat/BoatType.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Boat Type", menuName = "Boat Type")]
+public class BoatType : ScriptableObject
+{
+    public float health;
+    public int capacity;
+    public float speed;
+    public Mesh model;
+    public Material[] materials;
+}
