044a3ef7afa9ab45e4acc0462d762a6a3b696ce6
GlynLeine <445021@student.saxion.nl>
2019-09-08 22:26:48 +0200

    Start preperations for port to mobile, add toonColour effect, add outline shaders, add WaterV4 ( no normal map)

--- Unity Project/Hope/Assets/Scripts/Level Objects/LevelMasterScript.cs
+++ Unity Project/Hope/Assets/Scripts/Level Objects/LevelMasterScript.cs
@@ -12,21 +12,48 @@ public class LevelMasterScript : MonoBehaviour
     // Start is called before the first frame update
     void OnValidate()
     {
-        for(int i = 0; i < spawnables.Length; i++)
+        for (int i = 0; i < spawnables.Length; i++)
         {
             spawnables[i].spawnablePrefab.layer = LayerMask.NameToLayer("Interactable");
-            spawnables[i].spawnTime = 1f/spawnables[i].spawnRate;
+            spawnables[i].spawnTime = 1f / spawnables[i].spawnRate;
         }
 
         boat = FindObjectOfType<BoatScript>();
     }
 
+    private void Start()
+    {
+        List<FloatingObjectScript> spawnedObjects = new List<FloatingObjectScript>();
+        float simulatedDeltaTime = 0.5f;
+        for (int i = 0; i < 100; i++)
+        {
+            spawnedObjects.AddRange(SpawnObjects(simulatedDeltaTime));
+            foreach(FloatingObjectScript spawnedObject in spawnedObjects)
+            {
+                spawnedObject.transform.position = spawnedObject.transform.position - (Vector3.forward * simulatedDeltaTime * boat.boatSpeed * 3);
+            }
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
+       SpawnObjects();
+    }
+
+    private List<FloatingObjectScript> SpawnObjects(float simulatedDeltaTime = -1f)
+    {
+        float deltaTime;
+        if (simulatedDeltaTime <= 0)
+            deltaTime = Time.fixedDeltaTime;
+        else
+            deltaTime = simulatedDeltaTime;
+
+        List<FloatingObjectScript> spawnedObjects = new List<FloatingObjectScript>();
+
         foreach (SpawnInfo spawnInfo in spawnables)
         {
-            float spawnChance = Time.fixedDeltaTime/spawnInfo.spawnTime;
+            float spawnChance = deltaTime / spawnInfo.spawnTime;
 
             if (UnityEngine.Random.value <= spawnChance)
             {
@@ -34,12 +61,15 @@ public class LevelMasterScript : MonoBehaviour
                 newObject.transform.position = transform.position + (UnityEngine.Random.Range(-10f, 10f) * Vector3.right);
 
                 FloatingObjectScript floatingObjectScript = newObject.GetComponent<FloatingObjectScript>();
-                if(floatingObjectScript == null)
+                if (floatingObjectScript == null)
                     floatingObjectScript = newObject.AddComponent<FloatingObjectScript>();
 
                 floatingObjectScript.boat = boat;
+                spawnedObjects.Add(floatingObjectScript);
             }
         }
+
+        return spawnedObjects;
     }
 }
 
