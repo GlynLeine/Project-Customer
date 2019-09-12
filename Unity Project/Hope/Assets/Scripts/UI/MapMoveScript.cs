0ca53e5e603df9d6adc032549d5647f257a84086
Rick Oosthof <421183@student.saxion.nl>
2019-09-12 12:10:04 +0200

    updated level select/temp bg movement ground

---
+++ Unity Project/Hope/Assets/Scripts/UI/MapMoveScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapMoveScript : MonoBehaviour
+{
+    [SerializeField] private float CurrentX;
+    [SerializeField] private Slider mySlider;
+    
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        CurrentX = Mathf.Lerp(-1400, 1400, mySlider.value );
+        currentPosition.x = CurrentX;
+        transform.position = currentPosition;
+    }
+}
