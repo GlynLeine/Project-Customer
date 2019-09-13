e7c0429f6c6bd6f4cadc7b8833e022efa8f516f9
GlynLeine <445021@student.saxion.nl>
2019-09-13 17:27:34 +0200

    Add Time trigger, Add Start of Scene Trigger, Fix scriptableEvent not saving, Add ScriptableEventTrigger component, Add Scriptable Event Varient, Change scriptable Event to use components instead of scripts, Chance scriptable event component value type to use field name instead of field info.

--- Unity Project/Hope/Assets/Resources/Scripts/ScriptableEvents/ScriptedEvent.cs
+++ Unity Project/Hope/Assets/Resources/Scripts/ScriptableEvents/ScriptedEvent.cs
@@ -8,12 +8,30 @@ public enum EventType
     Dialogue
 }
 
+public enum TriggerType
+{
+    SceneStart, TimeInLevel, ScriptValue
+}
+
 [Serializable]
 public class EventTrigger
 {
-    public MonoBehaviour triggeringComponent;
-    public FieldInfo field;
+    public TriggerType triggerType;
+    public float triggerTime;
+    public Component triggeringComponent;
+    public string fieldName;
     public string value;
+
+    public virtual EventTrigger Copy()
+    {
+        EventTrigger copy = new EventTrigger();
+        copy.triggerType = triggerType;
+        copy.triggerTime = triggerTime;
+        copy.triggeringComponent = triggeringComponent;
+        copy.fieldName = fieldName;
+        copy.value = value;
+        return copy;
+    }
 }
 
 [Serializable]
@@ -23,8 +41,10 @@ public class TypedTrigger<T> : EventTrigger
 
     public TypedTrigger(EventTrigger source)
     {
+        triggerType = source.triggerType;
+        triggerTime = source.triggerTime;
         triggeringComponent = source.triggeringComponent;
-        field = source.field;
+        fieldName = source.fieldName;
         try
         {
             value = (T)Convert.ChangeType(source.value, typeof(T));
@@ -35,14 +55,48 @@ public class TypedTrigger<T> : EventTrigger
         }
         base.value = source.value;
     }
+
+    public override EventTrigger Copy()
+    {
+        TypedTrigger<T> copy = new TypedTrigger<T>(this);
+        return copy as EventTrigger;
+    }
 }
 
 [CreateAssetMenu(fileName = "New Scripted Event", menuName = "Scripted Event")]
 public class ScriptedEvent : ScriptableObject
 {
+    [HideInInspector]
+    public Component parent;
+
     public static string[] supportedTypes = { nameof(Boolean), nameof(Single), nameof(Int32), nameof(String) };
 
     public EventType eventType;
     [SerializeField, HideInInspector]
     public List<EventTrigger> eventTriggers;
+}
+
+[CreateAssetMenu(fileName = "New Scripted Event Variant", menuName = "Scripted Event Variant")]
+public class ScriptedEventVariant : ScriptedEvent
+{
+    [HideInInspector]
+    public ScriptedEvent source;
+
+    public void UpdateSource()
+    {
+        if (source != null)
+        {
+            eventType = source.eventType;
+            eventTriggers = new List<EventTrigger>();
+            foreach (EventTrigger eventTrigger in source.eventTriggers)
+            {
+                eventTriggers.Add(eventTrigger.Copy());
+            }
+        }
+        else
+        {
+            eventType = 0;
+            eventTriggers = null;
+        }
+    }
 }
---
+++ Unity Project/Hope/Assets/Resources/Scripts/ScriptableEvents/ScriptedEventTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptedEventTrigger : MonoBehaviour
+{
+    public ScriptedEvent scriptedEvent;
+    private ScriptedEvent prevScriptedEvent;
+    private void OnValidate()
+    {
+        if (scriptedEvent != null)
+        {
+            scriptedEvent.parent = this;
+            prevScriptedEvent = scriptedEvent;
+        }
+        else if(prevScriptedEvent != null)
+        {
+            prevScriptedEvent.parent = null;
+            prevScriptedEvent = null;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
