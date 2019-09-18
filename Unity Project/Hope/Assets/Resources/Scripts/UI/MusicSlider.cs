3d7a23961f895b8d1b33b7f8768e2deb60a03b6a
Rick Oosthof <421183@student.saxion.nl>
2019-09-18 15:12:41 +0200

    music slider

---
+++ Unity Project/Hope/Assets/Resources/Scripts/UI/MusicSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicSlider: MonoBehaviour
+{
+    public AudioMixer MusicMixer;
+    public void ChangeVol(float newValue)
+    {
+        MusicMixer.SetFloat("Volume", Mathf.Log(newValue) * 20);
+    }
+}
+
