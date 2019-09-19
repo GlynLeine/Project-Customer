8beb50bb6f85feea080717c8f1a785809984afce
GlynLeine <445021@student.saxion.nl>
2019-09-19 16:50:47 +0200

    edit trash collider

--- Unity Project/Hope/Assets/Resources/Scripts/ScriptableEvents/System/ScriptedEventTriggerEditor.cs
+++ Unity Project/Hope/Assets/Resources/Scripts/ScriptableEvents/System/ScriptedEventTriggerEditor.cs
@@ -2,7 +2,7 @@
 using UnityEditor;
  
 [CanEditMultipleObjects]
-[CustomEditor(typeof(ScriptedEventTrigger), true)]
+[CustomEditor(typeof(ScriptedEventTrigger))]
 public class ScriptedEventTriggerEditor : Editor
 {
 }
